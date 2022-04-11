2d8128902690edfff4360f198778b1226634c673
Bucosion <82382840+Bucosion@users.noreply.github.com>
2022-04-11 10:20:18 +0300

    Add files via upload

---
+++ Decorator/Decorator/BulgarianPizza.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+     class BulgerianPizza : Pizza
+     {
+          public BulgerianPizza()
+              : base("Pizza bulgara")
+          { }
+          public override int GetCost()
+          {
+               return 8;
+          }
+     }
+}
---
+++ Decorator/Decorator/TomatoPizza.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+     class TomatoPizza : PizzaDecorator
+     {
+          public TomatoPizza(Pizza p)
+              : base(p.Name + ", cu tomat", p)
+          { }
+
+          public override int GetCost()
+          {
+               return pizza.GetCost() + 3;
+          }
+     }
+}
